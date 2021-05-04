37f58d01bec144a91878923a1e588eda67bf353f
G0nov4 <garynova00@gmail.com>
2021-05-03 23:32:15 -0400

    Añadido registros

--- src/routes/autentication.js
+++ src/routes/autentication.js
@@ -4,6 +4,8 @@ const passport = require("passport");
 const { isLogged, isNotLogged } = require('../lib/auth')
 
 
+const pool = require('../database')
+
 // Signin . registrarse
 router.get("/signup", isNotLogged ,(req, res) => {
     res.render("auth/signup.hbs");
@@ -35,6 +37,44 @@ router.post("/signin", isNotLogged, (req, res, next)=>{
 router.get('/profile',isLogged, (req, res) => {
   res.render('./profile.hbs');
 });
+
+router.get('/profile/reported',isLogged, (req, res) => {
+  res.render('links/reported.hbs');
+});
+
+router.get('/profile/public',isLogged, (req, res) => {
+  res.render('links/public.hbs');
+});
+
+router.get('/profile/Encontrado',isLogged, (req, res) => {
+  res.render('links/Encontrado.hbs');
+});
+
+
+
+router.post('/profile/reported', isLogged, async (req, res)=> {
+
+  const { name_pet, specie, size, sex,  age, date, color, direction, observation} = req.body;
+  const statuss = 'reported';
+  const mapp = 'maaap1';
+  const newPet = {
+      iduser : 1,
+      name:name_pet, 
+      specie, 
+      size, 
+      sex, 
+      color, 
+      observation,
+      status: 'reported',
+      direction,
+      datePet:date,
+      map: 'ttt' 
+  };
+  await pool.query('Insert into pet set ?', [newPet]);
+ console.log(newPet) 
+  res.send('Aqui irá las mascotas perdidas...');
+}
+);
 
 router.get('/logout', (req, res)=>{
   req.logout();
